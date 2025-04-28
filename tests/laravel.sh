dbc035e0f15bc20140c6db1d53014556a857016c
Szobonya Csaba <opensource@szobonya.com>
2025-04-28 14:46:49 +0200

    test

---
+++ tests/laravel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+LARAVEL_VERSION_CONSTRAINT="${1:-^12.0}"
+
+echo "Install Laravel ${LARAVEL_VERSION_CONSTRAINT}"
+composer create-project --quiet --prefer-dist "laravel/laravel:${LARAVEL_VERSION_CONSTRAINT}" ../laravel
+cd ../laravel/
+SAMPLE_APP_DIR="$(pwd)"
+composer show --direct
+
+echo "Add pacakge from source"
+composer config minimum-stability dev
+ls -ls
+#composer config repositories.0 '{ "type": "path", "url": "../work", "options": { "symlink": false } }'
+
+# No version information with "type":"path"
+#composer require --dev --optimize-autoloader "csaba215/laravel-mnb:*"
